2552d18cf229753307d9967668d44f3bbdebc3e5
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-14 10:36:35 -0700

    Create state and api fetch for characters

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import Character from './components/Character'
 import './App.css';
 
 const App = () => {
@@ -9,9 +11,24 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+      axios.get(`https://swapi.dev/api/people`)
+      .then(function (response) {
+          console.log(response.data);
+          setCharacters(response.data);
+      })
+      .catch(function (error) {
+          console.log(error);
+      })
+  }, [])
+
+
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      {/* <Character characters={characters}/> */}
     </div>
   );
 }
