72f2d592a3796bb25ee4b9217cda58ba6ee12bf1
Josh Scanlan <75971516+joshuascan@users.noreply.github.com>
2021-05-14 11:14:04 -0700

    Add styled components

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,11 +1,31 @@
 // Write your Character component here
 import React from 'react'
-import Styled from 'styled-components'
+import styled from 'styled-components'
+
+const StyledCharacter = styled.div`
+    width: 70%;
+    text-align: left;
+    /* display: flex;
+    justify-content: left;
+    align-items: center; */
+    background-color: rgba(0,0,0,0.5);
+    padding: 2%;
+    margin: 4%;
+    box-shadow: 0px 0px 25px purple;
+
+    h2 {
+        font-size: 28px;
+        color: darkred;
+        text-shadow: 0px 0px 15px lightgray;
+    }
+`
 
 export default function Character({ character }) {
     return (
-        <div>
-        <h2>{character.name}</h2>
-        </div>
+        <StyledCharacter>
+        {/* <div> */}
+            <h2>{character.name}</h2>
+        {/* </div> */}
+        </StyledCharacter>
     );
 };
